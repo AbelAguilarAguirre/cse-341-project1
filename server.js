dd28c89801e4df21a44a5873196b6a021dd855c1
Abel Aguilar <ulysses.aguilar.aguirre@gmail.com>
2023-09-14 11:22:41 -1000

    Added Swagger

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-app.use(express.json());
-app.use(bodyParser.json());
+const app = express();
+
 const port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 app.use('/', require('./routes'));
 
 
